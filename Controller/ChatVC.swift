38b2ccae33f9d8427901d7c0fb0d4fa6bb62d8c6
Vibhanshu Vaibhav <vibhanshu_chaudhary@yahoo.co.in>
2018-11-06 22:09:32 +0530

    Added SWRevealViewController

---
+++ Controller/ChatVC.swift
@@ -0,0 +1,23 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Vibhanshu Vaibhav on 05/11/18.
+//  Copyright © 2018 Vibhanshu Vaibhav. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+    
+}
