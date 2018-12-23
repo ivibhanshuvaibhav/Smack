37f4cd5c13f2e1b66b1158dafb1050fb67bcb8e5
Vibhanshu Vaibhav <vibhanshu_chaudhary@yahoo.co.in>
2018-12-23 14:27:04 +0530

    Added SelectPersonVC

--- Controller/ChannelVC.swift
+++ Controller/ChannelVC.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class ChannelVC: UIViewController {
 
+    // Outlets
+    
+    @IBOutlet weak var loginBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.revealViewController()?.rearViewRevealWidth = self.view.frame.width - 60
-        self.revealViewController()?.frontViewShadowOpacity = 0
     }
 
+    @IBAction func loginBtnPressed(_ sender: Any) {
+        performSegue(withIdentifier: TO_LOGIN, sender: nil)
+    }
 }
---
+++ Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Vibhanshu Vaibhav on 01/12/18.
+//  Copyright © 2018 Vibhanshu Vaibhav. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+
+let TO_LOGIN = "toLogin"
